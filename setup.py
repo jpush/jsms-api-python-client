9977deae8004fcea387aa500fe31e580cc2f76dc
Helperhaps <helperhaps@users.noreply.github.com>
2018-01-12 06:08:35 +0000

    fresh

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+import jsms
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name = 'jsms',
+    version = jsms.__version__,
+    keywords = 'jiguang jsms python-sdk',
+    description = "JSMS\'s officially supported Python client library",
+    long_description = readme,
+    author = 'jpush',
+    author_email = 'support@jpush.cn',
+    url = 'https://github.com/jpush/jpush-api-python-client',
+    license = 'MIT',
+    packages = [ 'jsms' ],
+    install_requires = [ 'requests' ],
+    include_package_data = True,
+    zip_safe = False
+)
