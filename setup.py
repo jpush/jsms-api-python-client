3e6ccc3ad08c1f4ffc7caa8c07b70fc690eda346
wangjq <wangjq@jiguang.cn>
2020-03-17 16:43:29 +0800

    修复bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 import jsms
 
-with open('README.md') as f:
+with open('README.md',encoding='utf-8') as f:
     readme = f.read()
 
 setup(
@@ -17,7 +17,7 @@ setup(
     author_email = 'support@jpush.cn',
     url = 'https://github.com/jpush/jsms-api-python-client',
     license = 'MIT',
-    # packages = [ 'jsms' ],
+    py_modules = [ 'jsms' ],
     install_requires = [ 'requests' ],
     include_package_data = True,
     zip_safe = False
